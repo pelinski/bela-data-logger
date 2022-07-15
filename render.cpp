c2eb8d912ef2b837bafee226e355f0bb845a36ef
Teresa Pelinski <teresapelinski@gmail.com>
2022-07-15 20:46:01 +0100

    working logs

--- render.cpp
+++ render.cpp
@@ -1,3 +1,5 @@
+
+#include <libraries/WriteFile/WriteFile.h>
 #include <vector>
 #include "BelaParallelComm/BelaParallelComm.h"
 
@@ -5,46 +7,56 @@
 #define BELA_MASTER 0
 #endif
 
-#define VERBOSE 1
-
 bool gBelaIsMaster = (bool)BELA_MASTER;
 
 std::vector<unsigned int> gCommPins{ 0, 1, 2, 3 };
 
 BelaParallelComm parallelComm;
 
-const unsigned int kNumBlocksComm = 1;  // This example works only for one block of data, needs revision for
-                                        // multiple blocks
+const unsigned int kNumBlocksComm = 1;  // This example works only for one block of data, needs revision for multiple blocks
 const unsigned int kHeaderSizeComm = 0; // No header, all data
 
 unsigned int gCommBlockCount = 0;
 unsigned int gCommCount = 0;
 unsigned int gCommBlockSpan = 689; // ~ 0.25 sec @44.1k (16 block size)
 
-// Sensor analog pin
-unsigned int gSensorPin = 0;
+WriteFile dataLog;
+WriteFile syncLog;
 
 /*** SETUP ***/
 bool setup(BelaContext* context, void* userData) {
-    // If Bela is master...
-    if (gBelaIsMaster) {
 
-        parallelComm.setup(gCommPins.data(), gCommPins.size(),
-                           /* headerSize */ kHeaderSizeComm,
-                           /* nBlocks */ kNumBlocksComm);
-        parallelComm.printDetails();
-        parallelComm.prepareTx(context, 0);
-        parallelComm.printBuffers();
+    /** Setup data logger**/
+
+    if ((!context->flags) & BELA_FLAG_INTERLEAVED) {
+        fprintf(stderr, "DataLogger requires interleaved buffers\n");
+        return false;
     }
-    // If Bela is not master...
-    else {
 
-        parallelComm.setup(gCommPins.data(), gCommPins.size(),
-                           /* headerSize */ kHeaderSizeComm,
-                           /* nBlocks */ kNumBlocksComm);
-        parallelComm.printDetails();
+    dataLog.setup("data.log");      // set the file name to write to
+    dataLog.setEchoInterval(10000); // only print to the console every 10000 calls to log
+    dataLog.setFileType(kText);
+    dataLog.setHeader("");
+    dataLog.setFooter("");
+    dataLog.setFormat("%.0f, %.4f, %.4f, %.4f, %.4f\n"); // Output format. Use only %f (with modifiers). When in binary mode, this is used only for echoing the console.
+
+    syncLog.setup("sync.log");
+    syncLog.setEchoInterval(0);
+    syncLog.setFileType(kText);
+    syncLog.setHeader("");
+    syncLog.setFooter("");
+    syncLog.setFormat("%.0f, %.0f\n");
+
+    /**Setup parallel communication**/
+
+    parallelComm.setup(gCommPins.data(), gCommPins.size(), kHeaderSizeComm, kNumBlocksComm);
+    parallelComm.printDetails();
+    parallelComm.printBuffers(true);
+
+    if (gBelaIsMaster) {
+        parallelComm.prepareTx(context, 0);
+    } else {
         parallelComm.prepareRx(context, 0);
-        parallelComm.printBuffers();
     }
 
     return true;
@@ -52,47 +64,48 @@ bool setup(BelaContext* context, void* userData) {
 
 /*** RENDER ***/
 void render(BelaContext* context, void* userData) {
+    for (unsigned int n = 0; n < context->analogFrames; n++) { // ! analogFrames here instead of audioFrames
 
-    /* AUDIO LOOP */
-    for (unsigned int n = 0; n < context->audioFrames; n++) {
+        // Timestamp
+        unsigned int framesElapsed = n + context->audioFramesElapsed;
 
-        // Read sensor value
-        float sensorValue = analogRead(context, n, gSensorPin);
+        //**Log timestamp and sensor values into dataLog**//
+        dataLog.log(framesElapsed);                                      // timestamp
+        dataLog.log(&(context->analogIn[n * context->analogFrames]), 4); // sensor values
 
+        //**Parallel communication**//
         // If Bela is master..
         if (gBelaIsMaster) {
             // If first frame (beginning of block) and specific number of blocks have elapsed...
             if (n == 0 && ++gCommBlockCount > gCommBlockSpan) {
                 gCommBlockCount = 0;                           // reset block count
                 parallelComm.prepareDataToSend(0, gCommCount); // write count to buffer
-#ifdef VERBOSE
-
-                // unsigned int samplesElapsed = n + context->audioFramesElapsed;
-                rt_printf("---- master ----\n");
-                rt_printf("---- sensor value = %f ----\n", sensorValue);
+                parallelComm.sendData(context, n);             // send buffer
                 parallelComm.printBuffers(true);
-#endif                                                           /* VERBOSE */
-                parallelComm.sendData(context, n);               // send buffer
+
+                // Log every timestamp and block sent to syncLog
+                syncLog.log(framesElapsed);
+                syncLog.log(gCommCount);
+
+                rt_printf("%d, %d\n", framesElapsed, gCommCount);
+
                 if (++gCommCount > parallelComm.getMaxDataVal()) // increment count and check if it has changed
                     gCommCount = 0;
             }
-        } else {
+        } else { // If Bela is receiver...
             parallelComm.readData(context, n);
-            if (parallelComm.isReady() && parallelComm.hasChanged()) {
-#ifdef VERBOSE
-                rt_printf("---- slave ----\n");
-                unsigned int samplesElapsed = n + context->audioFramesElapsed;
-                rt_printf("---- samples elapsed = %d ----\n", samplesElapsed);
-                rt_printf("---- sensor value = %f ----\n", sensorValue);
-
+            if (parallelComm.isReady() && parallelComm.hasChanged() && (parallelComm.getBufferVal() >= -1)) {
+                gCommCount = parallelComm.getBufferVal();
                 parallelComm.printBuffers(true);
-#endif /* VERBOSE */
-                if (parallelComm.getBufferVal() >= -1)
-                    gCommCount = parallelComm.getBufferVal();
+
+                // Log timestamp and block received to syncLog
+                syncLog.log(framesElapsed);
+                syncLog.log(gCommCount);
+
+                rt_printf("%d, %d\n", framesElapsed, gCommCount);
             }
         }
     }
-    /* END OF AUDIO LOOP */
 }
 
 void cleanup(BelaContext* context, void* userData) {}
